9cfec796292b692a0cd7e1d68211e43cc99ad4a7
Abdur Rakib Rony <65207907+ronyfr3@users.noreply.github.com>
2020-12-30 10:12:28 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Form from './Component/Form'
+
+
+const App = () => {
+  return (
+    <div>
+       <Form/>
+    </div>
+  )
+}
+
+export default App
